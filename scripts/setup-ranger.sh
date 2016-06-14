7fa6c3dd778ecdc659554e0ec60b171eacb23517
Bryan Bende <bbende@apache.org>
2016-06-14 15:31:32 -0400

    Setting supported components in install.properties before setup is run

--- scripts/setup-ranger.sh
+++ scripts/setup-ranger.sh
@@ -16,6 +16,7 @@ sed -i '\|^db_root_password=| s|$|'${DB_ROOT_PASSWORD}'|g' ${INSTALL_PROPERTIES_
 sed -i '\|^db_password=| s|$|'${DB_PASSWORD}'|g' ${INSTALL_PROPERTIES_FILE}
 sed -i '\|^audit_db_password=| s|$|'${AUDIT_DB_PASSWORD}'|g' ${INSTALL_PROPERTIES_FILE}
 sed -i '\|^audit_solr_urls=| s|$|'${AUDIT_SOLR_URLS}'|g' ${INSTALL_PROPERTIES_FILE}
+sed -i '\|^policymgr_supportedcomponents=| s|$|'${SUPPORTED_COMPONENTS}'|g' ${INSTALL_PROPERTIES_FILE}
 
 RANGER_DIR=`ls -t $STAGE_DIR | grep ranger | grep admin | head -1`
 FULL_RANGER_DIR="$STAGE_DIR/$RANGER_DIR"
